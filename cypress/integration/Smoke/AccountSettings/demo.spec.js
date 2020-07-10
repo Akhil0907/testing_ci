85f51d04eaf215710af638730d7a71cf1b06a3fa
Akhil0907 <45623246+Akhil0907@users.noreply.github.com>
2020-07-10 13:03:47 +0530

    Update demo.spec.js

--- cypress/integration/Smoke/AccountSettings/demo.spec.js
+++ cypress/integration/Smoke/AccountSettings/demo.spec.js
@@ -15,9 +15,9 @@
         cy.visit('https://app.useriq.com/')
         
         //login with email and password stored in settings
-        cy.get('#user_email').should('not.be.hidden').type(akhil.manjunath@useriq.com,{log:false})
+        cy.get('#user_email').should('not.be.hidden').type("akhil.manjunath@useriq.com",{log:false})
  
-        cy.get('#user_password').should('not.be.hidden').type(Akhil2020@,{log:false})
+        cy.get('#user_password').should('not.be.hidden').type("Akhil2020@",{log:false})
  
         cy.get('.signin-btn').should('not.be.disabled').click()
 
