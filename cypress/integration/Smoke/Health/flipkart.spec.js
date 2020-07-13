e9f8344780bd3fde78b45c6c5ae9605814982434
Akhil0907 <45623246+Akhil0907@users.noreply.github.com>
2020-07-13 15:15:22 +0530

    Create flipkart.spec.js

---
+++ cypress/integration/Smoke/Health/flipkart.spec.js
@@ -0,0 +1,21 @@
+// <reference types="Cypress" />
+
+
+
+//T713 create a detailed four block announcement campaign
+
+describe('Create an announcement campaign', () => {
+    
+
+    
+ 
+
+    it('Smoke:-Create a detailed four block announcement campaign ', () => {
+        
+      cy.visit('flikpart.com')
+        })
+    
+    })
+       
+ 
+
