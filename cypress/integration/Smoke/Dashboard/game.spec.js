163a0ec021f098ac370a174590d504228543c39c
Akhil0907 <45623246+Akhil0907@users.noreply.github.com>
2020-07-13 15:14:11 +0530

    Create game.spec.js

---
+++ cypress/integration/Smoke/Dashboard/game.spec.js
@@ -0,0 +1,19 @@
+// <reference types="Cypress" />
+
+
+
+//T713 create a detailed four block announcement campaign
+
+describe('Create an announcement campaign', () => {
+    
+    
+  
+    it('Smoke:-Create a detailed four block announcement campaign ', () => {
+        
+       cy.visit('gamedebate.com')
+        })
+    
+    })
+       
+ 
+
