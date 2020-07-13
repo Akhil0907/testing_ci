51089e262d87a23e18feee25d0ff39bd3d8c0201
Akhil0907 <45623246+Akhil0907@users.noreply.github.com>
2020-07-13 16:32:56 +0530

    Create amd.spec.js

---
+++ cypress/integration/Smoke/SiteDropdown/amd.spec.js
@@ -0,0 +1,19 @@
+// <reference types="Cypress" />
+
+
+
+describe('Create an announcement campaign', () => {
+    
+   
+    it('Smoke:-amd ', () => {
+        
+   
+
+          cy.visit('https://www.amd.com/en')
+           
+        })
+    
+    })
+       
+ 
+
