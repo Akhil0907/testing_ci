8ef66ae42092513f5d3fac1098f9ad16e92f9370
Akhil0907 <45623246+Akhil0907@users.noreply.github.com>
2020-07-13 15:42:28 +0530

    Create covid.spec.js

---
+++ cypress/integration/Smoke/SiteSetting/covid.spec.js
@@ -0,0 +1,23 @@
+// <reference types="Cypress" />
+
+import AnnouncementCampaign from '../../../../pageObjects/Campaigns/CreateCampaigns/create_announcement_campaigns'
+
+//T713 create a detailed four block announcement campaign
+
+describe('Create an announcement campaign', () => {
+    
+   
+
+    it('Smoke:-Create a detailed four block announcement campaign ', () => {
+        
+     cy.visit('https://www.worldometers.info/coronavirus/country/india/')
+  
+        
+       
+           
+        })
+    
+    })
+       
+ 
+
