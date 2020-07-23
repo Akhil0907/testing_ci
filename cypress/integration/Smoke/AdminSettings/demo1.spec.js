de3d09ece355e9211b66f856b6e96e480ff93c93
Akhil0907 <45623246+Akhil0907@users.noreply.github.com>
2020-07-23 15:52:18 +0530

    Update demo1.spec.js

--- cypress/integration/Smoke/AdminSettings/demo1.spec.js
+++ cypress/integration/Smoke/AdminSettings/demo1.spec.js
@@ -5,7 +5,9 @@ describe('Create an announcement campaign', () => {
     
     it('Smoke:-Create a detailed four block announcement campaign ', () => {
         
-       cy.visit('https://www.nvidia.com/en-in/')
+       cy.visit('/')
+        
+        cy.contains('Oracle').should('be.visible')
         })
     
     })
